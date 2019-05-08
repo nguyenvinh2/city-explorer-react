4b8b32a74a25cc5f9a5bf204b645fa7d096f5713
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-08 09:23:56 -0700

    scaffold

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="city-explorer">
+        City Explorer
+      </header>
+    </div>
+  );
+}
+
+export default App;
