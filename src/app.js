680801261abbfacb29bfa1712e99b717316f9ab6
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-08 09:46:06 -0700

    component organization

--- src/app.js
+++ src/app.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import './app.css';
+import React, { Component } from 'react';
+import { Fragment } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="city-explorer">
-        City Explorer
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <Fragment>
+        <Intro/>
+        <Header/>
+        <Main/>
+      </Fragment>
+    )
+  }
 }
+
+class Intro extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header/>
+      </Fragment>
+    )
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>City Explorer</h1>
+    )
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Search/>
+        <Map/>
+        <Result/>
+      </Fragment>
+    )
+  }
+}
+
+class Search extends Component {
+
+}
+
+class Map extends Component {
+
+}
+
+class Result extends Component {
+
+}
+
+
 
 export default App;
