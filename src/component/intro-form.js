1a061c6aa9959356f58e5c4a5f00136ae22daa65
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-10 09:26:31 -0700

    changed to factor

--- src/component/intro-form.js
+++ src/component/intro-form.js
@@ -11,19 +11,19 @@ export class IntroForm extends Component {
       <form onSubmit={event => this.props.onBackendSubmit(event)}>
         <label>
           Enter the URL to your deployed back end, making sure to remove the trailing forward slash
-        <input type="text"  onChange = {event => this.props.onChangeBackend(event)} placeholder="Enter backend url" />
+        <input type="text" name="backend" placeholder="Enter backend url" />
         </label>
       </form>
     } else {
       backendForm = "";
     }
 
-    if(!this.props.geocodeBoolean) {
+    if(!this.props.geocodeBoolean && !localStorage.getItem('geocode')) {
       geocodeForm = 
       <form onSubmit={event => this.props.onGeocodeSubmit(event)} >
         <label>
           Enter your Geocoding API Key:
-        <input type="text" onChange = {event => this.props.onChangeGeocode(event)} placeholder="Enter your Google API Key" />
+        <input type="text" name="geocode" placeholder="Enter your Google API Key" />
         </label>
       </form>
     } else {
